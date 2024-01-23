3eaf59c114ad38dca35aed72afb8574deb81d31a
Dvyd <david.llopislaguna@gmail.com>
2024-01-23 22:29:24 +0100

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/app/services/auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs'; // Importa Subject de RxJS
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ITokenInfo, IUser } from '../interfaces/user.interface';
+import { ITokenInfo } from '../interfaces/user.interface';
 import { tap } from 'rxjs/operators';
 import { UserService } from './user.service';
 import { SignalRService } from './signalr.service';
@@ -11,7 +11,7 @@ import { StorageHelper } from '../helpers/storage.helper';
 import { StorageKeyEnum } from '../interfaces/enums/chat';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private http = inject(HttpClient);
@@ -19,16 +19,21 @@ export class AuthService {
   private signalrService = inject(SignalRService);
   public urlAPI = environment.urlAPI;
 
-  login(usuario: IUsuarioChat): Observable<ITokenInfo> {
-    return this.http.post<ITokenInfo>(this.urlAPI + '/Usuario/login', usuario)
+  public register(registerData: any) {
+    return this.http.post(this.urlAPI + '/Usuario/register', registerData);
+  }
+
+  public login(loginData: IUsuarioChat): Observable<ITokenInfo> {
+    return this.http
+      .post<ITokenInfo>(this.urlAPI + '/Usuario/login', loginData)
       .pipe(
         // Utiliza el operador tap para emitir el evento después de un inicio de sesión exitoso
         tap((response: ITokenInfo) => {
           // Guarda el token en el almacenamiento local después de un inicio de sesión exitoso
-          StorageHelper.setItem(StorageKeyEnum.Token, response.token)
+          StorageHelper.setItem(StorageKeyEnum.Token, response.token);
 
           // Almacena el usuario en el servicio después de un inicio de sesión exitoso
-          this.userService.updateUser(usuario);
+          this.userService.updateUser(loginData);
 
           // Conecta al servidor SignalR después de un inicio de sesión exitoso
           this.signalrService.connect();
@@ -36,29 +41,14 @@ export class AuthService {
       );
   }
 
-  logout(): void {
+  public logout(): void {
     // Elimina el usuario almacenado al cerrar sesión
     this.userService.clearUser();
-    
+
     // Elimina el token almacenado al cerrar sesión
     StorageHelper.removeItem(StorageKeyEnum.Token);
 
-    // Desconecta del servidor SignalR 
+    // Desconecta del servidor SignalR
     this.signalrService.disconnect();
   }
-
-  // getUserLogged(): IUser | null {
-  //   // Devuelve el usuario almacenado
-  //   return this.currentUser;
-  // }
-
-  // getUserEmail(): string | null {
-  //   // Devuelve el email del usuario almacenado
-  //   return this.currentUser ? this.currentUser.email : null;
-  // }
-
-  // getUserRole(): string | null {
-  //   // Devuelve el rol del usuario almacenado
-  //   return this.currentUser && this.currentUser.rol ? this.currentUser.rol : null;
-  // }
 }
